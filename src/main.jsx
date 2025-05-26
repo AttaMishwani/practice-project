69d7868d5bf8a7c3ca976b14317dc274be551bd8
AttaMishwani <atta.rehmanmishwani@gmail.com>
2025-05-26 13:40:49 +0500

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/practice-project">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
