cfb29a00c98b27435bf53fa19cebf9c170ee8540
Atta Ur Rehman <145256710+AttaMishwani@users.noreply.github.com>
2025-07-02 13:54:20 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,16 @@ import meme from "../assets/meme.jpg"; // Adjust the path as necessary
 const Home = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-black">
- <h1>anas ki gand lelo free main aur chopa bhi lagaiga</h1>
+ <h1>🕳️ The End.
+Aur kya umeed thi? iPhone milega?
+
+Nahi bhai, sirf sharam aur pashemani.
+Itna time barbaad karne par khud ko salute karo 👏
+
+Ab page band karo...
+aur zindagi pe dobara ghuroor mat karna. 😂
+
+</h1>
     </div>
   );
 };
