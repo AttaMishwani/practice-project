cd5847cf8059940c0d08258680b455bbb4c28ba7
Atta Ur Rehman <145256710+AttaMishwani@users.noreply.github.com>
2025-07-02 13:49:20 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,7 @@ import meme from "../assets/meme.jpg"; // Adjust the path as necessary
 const Home = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-black">
-      <img
-        src={meme} // Replace with your actual image path
-        alt="Centered Content"
-        className="max-w-full max-h-full object-contain"
-      />
+ <h1>anas ki gand lelo free main aur chopa bhi lagaiga</h1>
     </div>
   );
 };
